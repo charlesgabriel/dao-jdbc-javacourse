bd9dce8bb734d1acd257f883d3a95fc3306a2fce
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2026-03-05 09:47:33 -0300

    SellerDaoJDBC delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,7 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("= = = = = TEST 1: Seller -> findById = = = = =");
@@ -34,10 +36,18 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
-		System.out.println("\n= = = = = TEST 4: Seller -> update = = = = =");
+		System.out.println("\n= = = = = TEST 5: Seller -> update = = = = =");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+
+		System.out.println("\n= = = = = TEST 6 Seller -> delete = = = = =");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+
+		sc.close();
 	}
 }
